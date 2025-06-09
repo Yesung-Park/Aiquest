516c6689a09fd126239d5b9afc85a01ed1ec2b87
Yesung-Park <162130620+Yesung-Park@users.noreply.github.com>
2025-06-09 16:07:59 +0900

    주석 수정

--- app/src/main/java/com/example/aiquest/aiquest.java
+++ app/src/main/java/com/example/aiquest/aiquest.java
@@ -25,7 +25,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-//주석 새로 달았습니다.
+//??
 public class aiquest extends AppCompatActivity {
 
     private EditText etUserInput;
